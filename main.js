7ea03265d996d776d9c7bc9444194ed26f3b7433
bf_tsai <tsai19911026@gmail.com>
2023-08-19 10:10:32 +0800

    initialize and download bootstrap

---
+++ main.js
@@ -0,0 +1,13 @@
+import './assets/scss/all.scss';
+import './node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+
+
+console.log("Hello world!");
+
+//tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
